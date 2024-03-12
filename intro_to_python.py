5d450b3863653635ce3a67d085b8545239be42b2
Saeed-hakeem <123465815+Saeed-hakeem@users.noreply.github.com>
2024-03-12 15:56:50 +0300

    v1

---
+++ intro_to_python.py
@@ -0,0 +1,31 @@
+
+if 5 > 2:
+  print("Five is greater than two!")
+
+
+#variables
+  students_name="kamau"#string
+  students_age=20 #integer
+  students_height= 6.5#float
+  students_weight= 54
+  studentsismale=True #boolean
+  print(students_name,students_age,students_height,students_weight)
+
+
+
+  firstname="kamau"
+  second_name="100"
+  fullname=firstname+second_name
+  print(fullname)
+
+
+  price =  200
+  quantity = 5
+  total = price*quantity
+  print(total)
+
+  price1 = "200"
+  quantity = 5
+  total = int(price1) + quantity
+  print(total)
+
