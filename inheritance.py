9aaf973f350496e17705bca29f48864b134411ba
Saeed-hakeem <123465815+Saeed-hakeem@users.noreply.github.com>
2024-03-22 16:37:43 +0300

    inheritance v2

--- inheritance.py
+++ inheritance.py
@@ -6,11 +6,23 @@ class Person:
         self.person_age=person_age
         self.person_name=person_name
         self.person_residence=person_residence
-
-    def printPerson(self):
+    
+    def printPerson(self): #function to print properties
         print("Age:",self.person_age,"Name:",self.person_name,"Residence:",self.person_residence)
 
 #inheritance from the parent class
 class Student(Person):
-    def __init__(self,person_age,person_name,person_residence):
+    uniform=''
+    def __init__(self,person_age,person_name,person_residence,uniform):
         super().__init__(person_age,person_name,person_residence)
+        self.uniform=uniform
+
+class teacher(Person):
+    pay=0
+    def __init__(self,person_age,person_name,person_residence,pay):
+        super().__init__(person_age,person_name,person_residence)
+        self.pay=pay
+
+t1=teacher(100,"said","nairobi",200)
+t1.printPerson()
+print("pay:",t1.pay)
---
+++ inheritance2.py
@@ -0,0 +1,18 @@
+class Person:
+    person_age=''
+    person_name= ''
+    person_residence= ''
+    def __init__(self):
+        print("init person")
+
+    def set_values(self,person_age,person_name,person_residence):
+        self.person_age= person_age
+        self.person_name= person_name
+        self.person_residence= person_residence
+
+    def display_values(self):
+        print("The persons age is:",self.person_age,"The name is:",self.person_name,"The residence is:",self.person_residence)
+
+person1=Person()
+person1.set_values(50,"kelvin","kiserian")
+person1.display_values()
